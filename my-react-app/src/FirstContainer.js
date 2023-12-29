46c1dd70acd1e8fa0ca3d8913133e75816d60557
ohseungmin <ohmin2886@gmail.com>
2023-12-29 17:46:34 +0900

    첫번째 컨테이너 li태그에 names 요소가 들어갈 위치 지정

---
+++ my-react-app/src/FirstContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FirstContainer = () => {
+  const names = [
+    "김우진", "김현", "방승희", "변호녕", "소사무엘", "송영준", "신동현", "오승민",
+    "유승민", "윤준현", "이민구", "이유안", "이채이", "정영식", "최성민", "최은철", "홍문기"
+  ];
+
+
+  return (
+    <ul id="first-container">
+      {names.map((name) => (
+        <li>{name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default FirstContainer;
