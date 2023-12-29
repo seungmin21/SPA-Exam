1b3b991db2c4fa05519b2b55135874ce4091c679
ohseungmin <ohmin2886@gmail.com>
2023-12-29 18:40:45 +0900

    컨테이너 박스로 이동

---
+++ my-react-app/src/ContainerBox/ThirdContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ThirdContainer = () => {
+  return (
+    <div id="third-container">
+      <input type='text' placeholder='Write your name.'></input>
+      <button>show</button>
+    </div>
+  );
+};
+
+export default ThirdContainer;
